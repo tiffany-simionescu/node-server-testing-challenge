80550b99fd4f5d094f3aa9966d714115f340106d
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-02-05 22:46:11 -0500

    Met MVP

--- users/user-model.spec.js
+++ users/user-model.spec.js
@@ -7,14 +7,19 @@ beforeEach(async () => {
 
 describe("users model", () => {
 
-  test("add", async () => {
-    await userModel.add({ username: "tiffany88", password: "123456" })
-    const users = await db("users").select();
-    expect(users).toHaveLength(2)
+  test("find", async () => {
+    const res = await userModel.find();
+    expect(res.length).toBeGreaterThan(0);
   })
 
   test("findById", async () => {
     const res = await userModel.findById(1);
     expect(res.username).toBe("tiffany25")
+  })
+
+  test("add", async () => {
+    await userModel.add({ username: "tiffany88", password: "123456" })
+    const users = await db("users").select();
+    expect(users).toHaveLength(2)
   })
 })
