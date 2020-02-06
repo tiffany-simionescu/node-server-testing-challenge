6792a165807f8b1def932024f17993a59141e26b
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-02-05 19:35:44 -0500

    user-router.spec.js works

---
+++ users/user-router.spec.js
@@ -0,0 +1,34 @@
+const supertest = require('supertest');
+const router = require('./user-router');
+const server = require('../api/server');
+const db = require('../database/dbConfig');
+
+beforeEach(async () => {
+  await db.seed.run();
+})
+
+// POST - /users/register
+test("add a new user", async () => {
+  const res = await supertest(server.use(router))
+      .post("/users/register")
+      .send({ username: "tiffany87", password: "123456" });
+
+  // Status Code - unauthorized
+  expect(res.status).toBe(500)
+
+  // Data Format
+  expect(res.type).toBe("application/json")
+})
+
+// POST - /users/login
+test("login", async () => {
+  const res = await supertest(server.use(router))
+      .post("/users/login")
+      .send({ username: "tiffany87", password: "123456" });
+
+  // Status Code - unauthorized
+  expect(res.status).toBe(401)
+
+  // Data Format
+  expect(res.type).toBe("application/json")
+})
